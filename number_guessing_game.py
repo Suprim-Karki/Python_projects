d983c7fb3194e4bbd6dd10fbf8600a590fc2981f
Suprim-Karki <shupreem1911@gmail.com>
2025-04-20 20:32:08 +0545

    Added number of guesses

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,6 +1,7 @@
 ''' This is a number guessting game'''
 
 import random
+guess=1
 
 # random.randrange(1,11)     #doesm't include 11
 try:
@@ -10,12 +11,14 @@ try:
         quit()
 except ValueError:
     print("Input a whole number: ")
+    quit()
 
 while True:
     random_no = random.randint(1,top_range)     #includes top range too
     user_input=int(input("Guess the number: "))
     if user_input==random_no:
-        print("You won")
+        print(f"You won\nYou got it in {guess} guesses")
         break
     else:
+        guess+=1
         print(f"Correct answer was {random_no}\nTry Again\n")
