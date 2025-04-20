eb87c463ad86859361226f3bfeaf0073082834c9
Suprim-Karki <shupreem1911@gmail.com>
2025-04-20 20:39:52 +0545

    Initializing variables

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,11 @@
 ''' This is a Rock Papaer and Scissors game'''
+
+import random
+total_games=0
+number_of_wins=0
+number_of_draws=0
+choices=["Rock","Paper","Scissors"]
+
+while True:
+    random_no=random.randint(1,3)
+    user_choice=int(input("Enter 0 for Rock, 1 for Paper and 2 for scissors"))
