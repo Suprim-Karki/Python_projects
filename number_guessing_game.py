877b94c7b4268bd24664b5acfa424a9f7c8929b3
Suprim-Karki <shupreem1911@gmail.com>
2025-04-20 20:28:42 +0545

    Done

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -8,9 +8,14 @@ try:
     if top_range<=0:
         print("Input a number greater than 0: ")
         quit()
-    else:
-        random_no = random.randint(top_range)     #includes 11
-
-
 except ValueError:
     print("Input a whole number: ")
+
+while True:
+    random_no = random.randint(1,top_range)     #includes top range too
+    user_input=int(input("Guess the number: "))
+    if user_input==random_no:
+        print("You won")
+        break
+    else:
+        print(f"Correct answer was {random_no}\nTry Again\n")
