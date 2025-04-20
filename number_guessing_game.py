e01d8593ae420f8852a1ba41516da4edd1b253d5
Suprim-Karki <shupreem1911@gmail.com>
2025-04-20 20:15:15 +0545

    top range

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,5 +2,7 @@
 
 import random
 
+top_range = int(input("Enter the top range: "))
+
 # random.randrange(1,11)     #doesm't include 11
-x=random.randint(1,10)     #includes 11
+random_no = random.randint(1,10)     #includes 11
