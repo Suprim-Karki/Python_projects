cef8155176c16b93f69dc93f24f258367ef231ca
Suprim-Karki <shupreem1911@gmail.com>
2025-04-21 20:51:17 +0545

    Almost done

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,8 +4,24 @@ import random
 total_games=0
 number_of_wins=0
 number_of_draws=0
-choices=["Rock","Paper","Scissors"]
+choices=["rock","paper","scissors"]
 
 while True:
-    random_no=random.randint(1,3)
-    user_choice=int(input("Enter 0 for Rock, 1 for Paper and 2 for scissors"))
+    random_no=random.randint(0,2)
+    user_choice=input("\nEnter Rock/Paper/Scissors: (q to quit) ").lower()
+    if user_choice=="q":
+        break
+    if user_choice not in choices:
+        continue
+    comp_choice=choices[random_no]
+    if (comp_choice=="rock" and user_choice=="scissors") or (comp_choice=="scissors" and user_choice=="paper") or (comp_choice=="paper" and user_choice=="rock"):
+        print("\nYou lose!")
+        print(f"{comp_choice} beat your {user_choice}")
+    elif comp_choice==user_choice:
+        print("\nDraw")
+        number_of_draws+=1
+    else:
+        print("\nYou won")
+        print(f"Your {user_choice} beat {comp_choice}")
+        number_of_wins+=1
+    
