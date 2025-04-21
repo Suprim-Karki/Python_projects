dea8f4222ca22e936a9db73614646d409397b5db
Suprim-Karki <shupreem1911@gmail.com>
2025-04-21 21:15:09 +0545

    Choose your own adventure game start

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,7 @@
+''' Choose yourt own adventure game'''
+
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
