83960199c3003374c794b501ef40f46ed2116382
Suprim-Karki <shupreem1911@gmail.com>
2025-04-22 17:06:41 +0545

    Password manager while loop

--- password_manager.py
+++ password_manager.py
@@ -1,1 +1,14 @@
 ''' Password manager '''
+
+pwd=input("What is the master password: ")
+
+while True:
+    mode = input("\nEnter add or view to add/view passwords and q to quit: ").lower()
+    if mode=="q":
+        break
+    if mode=="view":
+        pass
+    elif mode=="add":
+        pass
+    else:
+        print("Invalid Input!\nTry Again")
