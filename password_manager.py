324e0604d48f020c2486340553dcde3a23d5096f
Suprim-Karki <shupreem1911@gmail.com>
2025-04-22 17:11:35 +0545

    Password manager 2 functions

--- password_manager.py
+++ password_manager.py
@@ -2,13 +2,24 @@
 
 pwd=input("What is the master password: ")
 
+def view():
+    with open("passwords.txt","r") as f:
+        f.read
+
+def add():
+    user=input("Enter username: ")
+    pwd=input("Enter password: ")
+
+    with open("passwords.txt","a") as f:
+        f.write(f"{user} | {pwd}\n")
+
 while True:
     mode = input("\nEnter add or view to add/view passwords and q to quit: ").lower()
     if mode=="q":
         break
     if mode=="view":
-        pass
+        view()
     elif mode=="add":
-        pass
+        add()
     else:
         print("Invalid Input!\nTry Again")
