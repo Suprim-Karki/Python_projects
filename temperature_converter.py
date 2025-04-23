1603abc4cc9129cc4b5f8b4e4a6c3a6053f7061b
Suprim-Karki <shupreem1911@gmail.com>
2025-04-23 21:45:04 +0545

    Temperature converter done

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,1 +1,9 @@
 ''' Temperature Converter '''
+
+def convert(s):
+    f = float(s)
+    c = (f - 32) * 5/9
+    return c
+
+temp=input("Enter temperature in fareenheit to convert into celsius: ")
+print(convert(temp))
