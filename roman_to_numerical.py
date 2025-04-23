e5e44a0443c7dc6b1521aafebc28a20a176154f5
Suprim-Karki <shupreem1911@gmail.com>
2025-04-23 21:49:31 +0545

    Roman number converter done

--- roman_to_numerical.py
+++ roman_to_numerical.py
@@ -1,1 +1,30 @@
 '''Convert roman number to decimal'''
+
+
+tallies = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
+
+def RomanNumeralToDecimal(romanNumeral):
+    sum = 0
+    for i in range(len(romanNumeral) - 1):
+        left = romanNumeral[i]
+        right = romanNumeral[i + 1]
+        if tallies[left] < tallies[right]:
+            sum -= tallies[left]
+        else:
+            sum += tallies[left]
+    sum += tallies[romanNumeral[-1]]
+    return sum
+
+try:
+    roman=input("Enter a roman number: ")
+    print(RomanNumeralToDecimal(roman))
+except ValueError:
+    print("Enter a valid input:\n")
