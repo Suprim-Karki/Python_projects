0dfa694a51118f91153aa4a678c7fa547a6d30c4
Suprim-Karki <shupreem1911@gmail.com>
2025-04-24 10:47:58 +0545

    madlibs generator completed

--- madlibs_generator.py
+++ madlibs_generator.py
@@ -1,4 +1,26 @@
 ''' Madlibs generator '''
 
 with open ("story.txt","r") as f:
-    
+    story=f.read()
+
+start_word=-1
+target_start="<"
+target_end=">"
+words=set()
+
+for i,char in enumerate(story):
+    if char==target_start:
+        start_word=i
+    if char==target_end and start_word!=-1:
+        word=story[start_word: i+1]
+        words.add(word)
+        start_word+=-1
+
+answers={}
+for word in words:
+    answer=input(f"Enter a word for {word}: ")
+    answers[word]=answer
+
+for word in words:
+    story = story.replace(word,answers[word])
+print(f"\n{story}")
