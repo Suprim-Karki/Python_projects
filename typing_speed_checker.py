220c07d0d6d67a6e56c9cf7d33f2e4cbf7b5700c
Suprim-Karki <shupreem1911@gmail.com>
2025-04-25 18:55:19 +0545

    Adding correct/mistake text

--- typing_speed_checker.py
+++ typing_speed_checker.py
@@ -10,20 +10,35 @@ def start_screen(stdscr):
     stdscr.addstr("\nPress enter to continue: ")   # 1,0 is the row and column 1 line down, 0 letter right
     stdscr.getkey()    # prevents program from immediately closing and stores what user typed
 
+def display_text(stdscr,target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1,0,f"WPM: {wpm}")
+    for i,char in enumerate(current):
+        correct_char=target[i]
+        color = curses.color_pair(1)
+        if char!=correct_char:
+            color=curses.color_pair(2)
+        stdscr.addstr(0,i,char,color)
+    
+
+
 def wpm_test(stdscr):
     target_text="Hello World this is a test."
     current_text=[]
-
-    stdscr.clear()
-    stdscr.addstr(target_text)
-    stdscr.refresh()
+    wpm=0
 
     while True:
+        stdscr.clear()
+        display_text(stdscr,target_text,current_text,wpm)
+        stdscr.refresh() 
         key = stdscr.getkey()
-        current_text.append(key)
-        for char in current_text:
-            stdscr.addstr(char,curses.color_pair(1))
-        stdscr.refresh()
+        if ord(key)==27:
+            break
+        if key in ("KEY_BACKSPACE","\b","\x7f"):
+            if len(current_text)>0:
+                current_text.pop()
+        elif len(current_text)<len(target_text):
+            current_text.append(key)
 
 def main(stdscr):   #stdscr = standard screen
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)    #bg and fg color
