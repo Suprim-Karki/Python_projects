5fdbe0a606a88d34ecceaadafc0ecf1f5a063878
Suprim-Karki <shupreem1911@gmail.com>
2025-04-28 13:05:36 +0545

    Fibonacci series done

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,23 @@
 ''' Fibonacci series '''
+def  fibonacci(n):
+
+	if n == 1  or n == 0:
+
+		return n;
+
+	else:
+
+		return fibonacci(n-2) + fibonacci(n - 1)
+
+
+numero = int(input("ingrese un numero entero  positivo: "))
+
+if numero < 0:
+	print("Number invalid")
+
+i = 0
+
+print("Fibonacci sequence is: ")
+
+for i in range(0, numero):
+	print(fibonacci(i))
